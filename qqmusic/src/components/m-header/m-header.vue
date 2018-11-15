7421c7faa149efee51e74dd4248cf91501db04f9
FENGXIUWEI <1428915751@qq.com>
2018-11-15 10:50:14 +0800

    m-header

---
+++ qqmusic/src/components/m-header/m-header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">QQ Music</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'm-header'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+  .m-header
+    height:3.75rem
+    text-align:center
+    position:relative
+    padding-top:0.5rem
+    font-size:0rem
+    .icon
+      display: inline-block
+      width:3.0625rem
+      height:3.3125rem
+      bg-image('logo')
+      background-size: 3.0625rem 3.3125rem
+      vertical-align:bottom
+    .text
+      display: inline-block
+      font-size:1.5rem
+      height:3.3125rem
+      line-height:3.3125rem
+      color:#ffcd32
+      vertical-align:bottom
+      margin-left:0.9375rem
+
+</style>
--- qqmusic/src/main.js
+++ qqmusic/src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+/* 此组件可以使用es6 进行开发 */
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+
+import '@/common/stylus/index.styl'
 
 Vue.config.productionTip = false
+
+/* 此组件是解决click 300 ms 延迟的问题 */
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
